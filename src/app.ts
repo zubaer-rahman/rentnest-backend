71fa41de715f8c131cd997c6319913b43f339f04
zubaer-rahman <zubaer.rahman.cse@gmail.com>
2026-07-06 14:45:00 +0600

    Setup main API router and link authentication module

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, json, Request, Response, urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import router from "./routes";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+import notFound from "./middlewares/notFound";
+
 const app: Application = express();
 
 app.use(
@@ -14,8 +18,13 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use("/api", router);
+
 app.get("/", (req:Request, res: Response)=>{
-    res.send("Hello World!")
+    res.send("Hello World! RentNest API is running.")
 })
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
---
+++ src/routes/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+];
+
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
